587d93545207de70b37774274f8accc082c96e7d
axmadJ3 <axmabdukarimov0304@outlook.com>
2025-06-27 16:28:46 +0500

    Add enroll action to CourseViewSet for student enrollment

--- educa/courses/api/views.py
+++ educa/courses/api/views.py
@@ -1,5 +1,9 @@
 from django.db.models import Count
 from rest_framework import viewsets
+from rest_framework.response import Response
+from rest_framework.decorators import action
+from rest_framework.authentication import BasicAuthentication
+from rest_framework.permissions import IsAuthenticated
 
 from courses.models import Subject, Course
 from courses.api.pagination import StandartPagination
@@ -16,3 +20,14 @@ class CourseViewSet(viewsets.ReadOnlyModelViewSet):
     queryset = Course.objects.prefetch_related('modules')
     serializer_class = CourseSerializer
     pagination_class = StandartPagination
+
+    @action(
+        detail=True,
+        methods=['post'],
+        authentication_classes=[BasicAuthentication],
+        permission_classes=[IsAuthenticated],
+    )
+    def enroll(self, request, *args, **kwargs):
+        course = self.get_object()
+        course.students.add(request.user)
+        return Response({'enrolled': True})
