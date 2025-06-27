14f574d4c08de0c214b2f217ab5bdf07a933c9f9
axmadJ3 <axmabdukarimov0304@outlook.com>
2025-06-27 14:54:37 +0500

    Add pagination support with StandartPagination class for API views

--- educa/courses/api/views.py
+++ educa/courses/api/views.py
@@ -1,13 +1,15 @@
 from rest_framework import generics
 from django.db.models import Count
 
-from courses.api.serializers import SubjectSerializer
 from courses.models import Subject
+from courses.api.pagination import StandartPagination
+from courses.api.serializers import SubjectSerializer
 
 
 class SubjectListView(generics.ListAPIView):
     queryset = Subject.objects.annotate(total_courses=Count('courses'))
     serializer_class = SubjectSerializer
+    pagination_class = StandartPagination
 
 
 class SubjectDetailView(generics.RetrieveAPIView):
