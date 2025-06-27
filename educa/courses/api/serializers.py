aca34a0d6bb5011acf94634d8f565c234198d2a2
axmadJ3 <axmabdukarimov0304@outlook.com>
2025-06-27 14:47:00 +0500

    Add popular courses field to SubjectSerializer with student count annotation

--- educa/courses/api/serializers.py
+++ educa/courses/api/serializers.py
@@ -1,11 +1,22 @@
 from rest_framework import serializers
 
+from django.db.models import Count
+
 from courses.models import Subject
 
 
 class SubjectSerializer(serializers.ModelSerializer):
     total_courses = serializers.IntegerField()
+    popular_courses = serializers.SerializerMethodField()
     
     class Meta:
         model = Subject
-        fields = ['id', 'title', 'slug', 'total_courses']
+        fields = ['id', 'title', 'slug', 'total_courses', 'popular_courses']
+
+    def get_popular_courses(self, obj):
+        courses = obj.courses.annotate(
+            total_students = Count('students')
+        ).order_by('total_students')[0:3]
+        return [
+            f'{c.title} ({c.total_students})' for c in courses
+        ]
