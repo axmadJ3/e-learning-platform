806e51b8ae3552ed6d0b443af89c10ec2a2f05a0
axmadJ3 <axmabdukarimov0304@outlook.com>
2025-06-30 00:48:31 +0500

    Add CHANNEL_LAYERS configuration for Redis support and update dependencies

--- educa/educa/settings.py
+++ educa/educa/settings.py
@@ -131,6 +131,17 @@ REST_FRAMEWORK = {
     ]
 }
 
+
+CHANNEL_LAYERS = {
+    'default': {
+        'BACKEND': 'channels_redis.core.RedisChannelLayer',
+        'CONFIG': {
+            'hosts': [('127.0.0.1', 6379)],
+        },
+    },
+}
+
+
 # Internationalization
 # https://docs.djangoproject.com/en/5.0/topics/i18n/
 
