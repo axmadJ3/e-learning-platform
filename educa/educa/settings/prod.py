743be52742f3ae0feead5af69755d53cd753efb1
axmadJ3 <axmabdukarimov0304@outlook.com>
2025-06-30 23:47:47 +0500

    Add Docker configuration files and update production settings for PostgreSQL and Redis

--- educa/educa/settings/prod.py
+++ educa/educa/settings/prod.py
@@ -1,3 +1,5 @@
+from decouple import config
+
 from .base import *
 
 
@@ -14,7 +16,15 @@ ALLOWED_HOSTS = ['*']
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': config('POSTGRES_DB'),
+        'USER': config('POSTGRES_USER'),
+        'PASSWORD': config('POSTGRES_PASSWORD'),
+        'HOST': 'db',
+        'PORT': 5432,
     }
 }
+
+REDIS_URL = 'redis://cache:6379'
+CACHE['default']['LOCATION'] = REDIS_URL
+CHANNEL_LAYERS['default']['CONFIG']['hosts'] = [REDIS_URL]
